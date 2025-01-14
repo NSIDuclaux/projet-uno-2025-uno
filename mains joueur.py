f4c08e7b8cc9e1fe23af53aa705c3f964b8ae46d
Quentin-Fly <quentin.chambrelavergne@gmail.com>
2025-01-14 12:01:03 +0100

    Modification de main

--- mains joueur.py
+++ mains joueur.py
@@ -1,19 +1,17 @@
 from random import randint
-from Deck import Deck
+from deck import Deck
 class Main :
 
-    def __init__ (self, Deck):
-        self.Deck = Deck
+    def __init__ (self, deck):
+        self.deck = deck
 
-    def main (Deck):
+    def main (deck):
 
-        nb = 0
-        main = []
         for i in range(7):
-            i = randint(108)
-            nb = Deck[i]
-            main.append(nb)
-            Deck.remove(i)
+
+            carte = Deck[0]
+            main.append(carte)
+            Deck.remove(0)
 
         
     def __str__(self):
@@ -23,5 +21,6 @@ class Main :
     def retirer (num,main):
         main.remove(num)
 
-main = Main(Deck)
-print(main)
+deck = Deck
+#main = 
+#print(main)
