faf712931942011153f55aca7f0d3c21f0611f77
Mario-de-Nsi <zstzbanmolina953@gmail.com>
2025-01-16 15:20:55 +0100

    pile update

--- Pile.py
+++ Pile.py
@@ -1,22 +1,38 @@
-class Pile:
-
-	def __init__(self):
-		self.valeurs = []
-
-	def empiler(self, valeur):
-		self.valeurs.append(valeur)
-
-	def depiler(self):
-		if self.valeurs:
-			return self.valeurs.pop()
-
-	def estVide(self):
-		return self.valeurs == []
+"""Implémentation des piles avec des listes Python"""
 
 
-	def __str__(self):
-		ch = ''
-		for x in self.valeurs:
-			ch = "|\t" + str(x) + "\t|" + "\n" + ch
-		ch = "\nEtat de la pile:\n" + ch
-		return ch
+def creer():
+    """Retourne une pile vide"""
+    return []
+
+
+def taille(pile):
+    """Retourne le nombre d'éléments de la pile"""
+    return len(pile)
+
+
+def est_vide(pile):
+    """Retourne True si la pile est vide, False sinon"""
+    return pile == []
+
+
+def empiler(pile, element):
+    """Empile un nouvel élément au sommet de la pile"""
+    pile.append(element)
+
+
+def depiler(pile):
+    """Retourne l'élément situé au sommet de la pile
+    et le supprime de celle-ci"""
+    if not est_vide(pile):
+        return pile.pop()
+    else:
+        return None
+
+
+def sommet(pile):
+    """Retourne l'élément situé au sommet de la pile"""
+    if not est_vide(pile):
+        return pile[-1]
+    else:
+        return None
