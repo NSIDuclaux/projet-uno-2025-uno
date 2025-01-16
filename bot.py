ac898043187aa29e38bb5d1b66081777cbaca0f8
Quentin-Fly <quentin.chambrelavergne@gmail.com>
2025-01-16 15:39:46 +0100

    Création du __init__

--- bot.py
+++ bot.py
@@ -2,6 +2,11 @@ from random import randint
 
 class Bot :
 
-    def __init__(self, ):
+    def __init__(self, mainJoueur, carteMilieu):
         
+        self.mainJoueur = mainJoueur
+        self.carteMilieu = carteMilieu
+
+    def jouer_carte (self):
+
         
