1e94cdd8aa37af1f1d83cb5984ae6a00d1627134
NicoooooMoulagauffre <totakearisk9@gmail.com>
2025-01-16 14:45:05 +0100

    h

---
+++ Pile.py
@@ -0,0 +1,22 @@
+class Pile:
+
+	def __init__(self):
+		self.valeurs = []
+
+	def empiler(self, valeur):
+		self.valeurs.append(valeur)
+
+	def depiler(self):
+		if self.valeurs:
+			return self.valeurs.pop()
+
+	def estVide(self):
+		return self.valeurs == []
+
+
+	def __str__(self):
+		ch = ''
+		for x in self.valeurs:
+			ch = "|\t" + str(x) + "\t|" + "\n" + ch
+		ch = "\nEtat de la pile:\n" + ch
+		return ch
