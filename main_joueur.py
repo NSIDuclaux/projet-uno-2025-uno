1c0a4132a956aea3ca475e2408555f39f244199c
Mario-de-Nsi <zstzbanmolina953@gmail.com>
2025-01-16 14:50:00 +0100

    longeur 1

--- main_joueur.py
+++ main_joueur.py
@@ -18,6 +18,9 @@ class Main :
         self.num_carte = num_carte
         self.main_joueur.pop(num_carte)
 
+    def nb_main(self, lg_main):
+        self.lg_main = len(self.main_joueur)
+        return self.lg_main
 
     def __str__(self):
 
