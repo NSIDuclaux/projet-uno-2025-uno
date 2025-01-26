bb1c7a500c52edf99bdf2422f6da434aa1db3132
Quentin-Fly <quentin.chambrelavergne@gmail.com>
2025-01-26 16:20:40 +0000

    EffetCarte Terminé

--- carte_effet.py
+++ carte_effet.py
@@ -1,18 +1,30 @@
 class EffetCarte :
 
-    def __init__ (self,carte,main,deck):
+    def __init__ (self,carte,main,deck,sensHoraire,pouvoirJouer):
 
         self.carte = carte
         self.main = main
         self.deck = deck
+        self.sensHoraire = sensHoraire
+        self.pouvoirJouer = pouvoirJouer
 
     def inverse (self):
 
-        
+        if self.sens_horaire == True:
+
+            self.sens_horaire = False
+
+        else :
+
+            self.sens_horaire = True
+
+        return self.sens_horaire
 
     def interdit_jouer (self):
 
-        return False
+        self.pouvoirJouer = False
+
+        return self.pouvoirJouer
 
     def plus_2_carte (self):
 
@@ -23,13 +35,22 @@ class EffetCarte :
 
     def changer_couleur(self):
 
-        pass
+        nouvelleCouleur = ""
+
+        while nouvelleCouleur != "jaune" or nouvelleCouleur != "rouge" or nouvelleCouleur != "bleu" or nouvelleCouleur != "vert":
+            nouvelleCouleur = str(input("Choissez une nouvelle couleur")).lower()
 
     def plus_4_carte (self):
-
-        #utilisé changé de couleur + 
 
         for i in range (4):
 
             self.main.ajouter_carte(self.deck.retirer_carte())
+
+        nouvelleCouleur = ""
+
+        while nouvelleCouleur != "jaune" or nouvelleCouleur != "rouge" or nouvelleCouleur != "bleu" or nouvelleCouleur != "vert":
+            
+            nouvelleCouleur = str(input("Choissez une nouvelle couleur")).lower()
+
+
 
