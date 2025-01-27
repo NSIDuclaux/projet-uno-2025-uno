43af637f4b28b2d48e8e6fab32d6e592d9791d1e
Quentin-Fly <quentin.chambrelavergne@gmail.com>
2025-01-27 09:42:17 +0100

    Modif de deck

--- deck.py
+++ deck.py
@@ -9,24 +9,43 @@ class Deck:
     def remplir_entier(self):
 
         for i in range(2):
+
             for c in range(4):
+            
                 for n in range(9):
-                    self.list_allc = self.list_allc + [str(Carte(c,n+1))]
+
+                    
+                    self.list_allc = self.list_allc + [(Carte(c,n+1))]
+        
         for c in range(4):
-            self.list_allc = self.list_allc + [str(Carte(c,0))]
+
+            self.list_allc = self.list_allc + [(Carte(c,0))]
+        
         for c in range(4):
+            
             for k in range(2):
-                self.list_allc = self.list_allc + [str(Carte(c,12))]
+                
+                self.list_allc = self.list_allc + [(Carte(c,12))]
+        
         for c in range(4):
+            
             for k in range(2):
-                self.list_allc = self.list_allc + [str(Carte(c,10))]
+                
+                self.list_allc = self.list_allc + [(Carte(c,10))]
+        
         for c in range(4):
+            
             for k in range(2):
-                self.list_allc = self.list_allc + [str(Carte(c,11))]
+                
+                self.list_allc = self.list_allc + [(Carte(c,11))]
+       
         for k in range(4):
-            self.list_allc = self.list_allc + [str(Carte(4,14))]
+            
+            self.list_allc = self.list_allc + [(Carte(4,14))]
+        
         for k in range(4):
-            self.list_allc = self.list_allc + [str(Carte(4,13))]
+            
+            self.list_allc = self.list_allc + [(Carte(4,13))]
 
     def melange(self):
 
@@ -39,21 +58,29 @@ class Deck:
     def ajouter_carte(self, carte):
 
         self.list_allc.append(carte)
+
+    def choix_carte(self, id):
+
+        return self.list_allc[id]
+        
         
     def __str__(self):
 
         self.affichage = []
 
-        for i in range (len(self.main_joueur)):
+        for i in range (len(self.list_allc)):
 
-            element = str(i+1) + " | " + str(self.main_joueur[i])
+            element = str(i+1) + " | " + str(self.list_allc[i])
             self.affichage.append(str(element))
 
         return str(self.affichage)
         
-# d = Deck()
-# d.remplir_entier()
-# d.melange()
-# print(d.retirer_carte())
-# print(d)
+d = Deck()
+d.remplir_entier()
+d.melange()
+print(d.retirer_carte())
+print(d)
+carte = d.choix_carte(2)
+print(carte)
+print(carte.get_couleur())
 
